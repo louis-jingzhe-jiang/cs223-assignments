501ba1551da9d2fd4ba006dff915009f95069772
Louis Jiang <louisjiang@wc115-dhcp-172.brynmawr.edu>
2023-01-23 15:54:37 -0500

    Mon, Jan 23 Modification; almost done

--- A01/mathgame.c
+++ A01/mathgame.c
@@ -1,11 +1,34 @@
 /***************************************************
  * mathgame.c
- * Author: 
+ * Author: Louis Jiang 
  * Implements a math game
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
+  int rounds = 0;
+  int correct = 0;
+  printf("Welcome to Math Game!\n");
+  printf("How many rounds do you want to play? \n");
+  scanf(" %d", &rounds);
+  printf("\n");
+  for (int i = 0; i < rounds; i++) {
+    int a = rand() % 100;
+    int b = rand() % 100;
+    int sum = a + b;
+    printf("\n%d + %d = ?\n", a, b);
+    int ans = 0;
+    scanf(" %d", &ans);
+    if (ans == sum) {
+      printf("\nCorrect :)\n");
+      correct++;
+    } else {
+      printf("\nIncorrect :(\n");
+    }
+  }
+  printf("\nYou answered %d/%d correctly.\n", correct, rounds);
   return 0;
 }
+
