e0edcf15be84c14657c02d76ff6680a15ca4dfac
louis-jingzhe-jiang <ljiang1@haverford.edu>
2023-01-24 17:18:59 -0500

    Tue, Jan 24; Memory check passed, optimized random number generation, PROJECT DONE

--- A01/mathgame.c
+++ A01/mathgame.c
@@ -6,22 +6,25 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main() {
-  int rounds = 0;
-  int correct = 0;
+  time_t t;
+  srand((unsigned) time(&t)); // initialize the random variable;
+  int rounds = 0; // the number of rounds
+  int correct = 0; // the number of correct answers
   printf("Welcome to Math Game!\n");
   printf("How many rounds do you want to play? \n");
-  scanf(" %d", &rounds);
+  scanf(" %d", &rounds); // input the number of rounds
   printf("\n");
   for (int i = 0; i < rounds; i++) {
     int a = rand() % 100;
     int b = rand() % 100;
-    int sum = a + b;
+    int sum = a + b; // the right answer
     printf("\n%d + %d = ?\n", a, b);
     int ans = 0;
-    scanf(" %d", &ans);
-    if (ans == sum) {
+    scanf(" %d", &ans); // input the answer for the question
+    if (ans == sum) { // compare the user's answer to the right answer
       printf("\nCorrect :)\n");
       correct++;
     } else {
