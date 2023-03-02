505b18be195223933d9d72ca4beaebe18d3b440a
louis-jingzhe-jiang <ljiang1@haverford.edu>
2023-03-01 20:59:38 -0500

    All done

--- A06/encode.c
+++ A06/encode.c
@@ -33,40 +33,47 @@ int main(int argc, char** argv) {
   char* phrase = malloc(sizeof(char) * num_char);
   scanf(" %s", phrase);
   phrase[num_char - 1] = '\0'; // make sure the phrase ends with '\0'
+  int phrlen = strlen(phrase); // the actual length of the phrase
   printf("If your phrase is longer than %i, ", num_char - 1);
   printf("only the first %i characters in your phrase ", num_char - 1);
   printf("will be encoded\n");
   // start to process the image to include the encoded message
+  // for unsigned char* raw:
+  // each char in the array stores only 1 bit of the data to be encoded
   unsigned char* raw = malloc(sizeof(unsigned char) * 8 * num_char);
   for (int i = 0; i < num_char; i++) {
     for (int j = 0; j < 8; j++) {
+      // 
       raw[i * 8 + j] = (phrase[i] >> (7 - j)) & 1;
       // printf("%i", raw[i * 8 + j]); // DEBUG
     }
   }
   printf("\n");
-  for (int i = 0; i < num_char * 8; i++) {
+  // only encode the data that is before the '\0' char
+  for (int i = 0; i < (phrlen + 1) * 8; i++) {
     // printf("Before: %i %i %i\n", pixels[i / 3].red, pixels[i / 3].green,
     //     pixels[i / 3].blue); // DEBUG
-    if (i % 3 == 0) {
-      if ((pixels[i / 3].red & 1) != raw[i]) {
-        if (pixels[i / 3].red == 255) {
+    // printf("%i red:%i green:%i blue:%i\n", i / 3, pixels[i/3].red, 
+    //    pixels[i/3].green, pixels[i/3].blue); // DEBUG
+    if (i % 3 == 0) { // this bit should be stored in red channel
+      if ((pixels[i / 3].red & 1) != raw[i]) { // not same bit, modify
+        if (pixels[i / 3].red == 255) { // prevent overflow
           pixels[i / 3].red -= 1;
         } else {
           pixels[i / 3].red += 1;
         }
       }
-    } else if (i % 3 == 1) {
-      if ((pixels[i / 3].green & 1) != raw[i]) {
-        if (pixels[i / 3].green == 255) {
+    } else if (i % 3 == 1) { // this bit should be stored in green channel
+      if ((pixels[i / 3].green & 1) != raw[i]) { // not same bit, modify
+        if (pixels[i / 3].green == 255) { // prevent ovlerflow
           pixels[i / 3].green -= 1;
         } else {
           pixels[i / 3].green += 1;
         }  
       } 
-    } else {
-      if ((pixels[i / 3].blue & 1) != raw[i]) {
-        if (pixels[i / 3].blue == 255) {
+    } else { // this bit should be stored in the blue channel
+      if ((pixels[i / 3].blue & 1) != raw[i]) { // not same bit, modify
+        if (pixels[i / 3].blue == 255) { // prevent overflow
           pixels[i / 3].blue -= 1;
         } else {
           pixels[i / 3].blue += 1;
@@ -77,10 +84,12 @@ int main(int argc, char** argv) {
     //     pixels[i / 3].blue); // DEBUG
   }
   // write out the new image
-  char* new_filename = malloc(sizeof(char) * (strlen(argv[1] + 8)));
-  for (int i = 0; i < strlen(argv[1]) - 4; i++) {
+  char* new_filename = malloc(sizeof(char) * (strlen(argv[1]) + 9));
+  int i = 0;
+  for (i = 0; i < strlen(argv[1]) - 4; i++) {
     new_filename[i] = argv[1][i];
   }
+  new_filename[i] = '\0';
   strcat(new_filename, "-encoded.ppm");
   printf("Writing file %s\n", new_filename);
   write_ppm(new_filename, pixels, w, h);
