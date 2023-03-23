b206e97c2982cd6e5653d61f15772719ad397564
Louis Jiang <louisjiang@Mono.local>
2023-03-23 08:55:41 -0400

    debug

--- A08/single_mandelbrot.c
+++ A08/single_mandelbrot.c
@@ -1,3 +1,8 @@
+/**
+ * Author: Louis Jiang
+ * Date: Mar 22, 2023
+ * Description: A program that outputs a PPM image of the mandelbrot set
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -5,6 +10,21 @@
 #include <sys/time.h>
 #include "read_ppm.h"
 #include "write_ppm.h"
+#include <string.h>
+
+
+/**
+ * Find the index of the pixel in the array of pixels with its row number
+ * and column number, given the number of columns of the array.
+ * @param row the row number
+ * @param col the column number
+ * @param num_col the number of columns in the array
+ * @return the index of the pixel
+ */
+int find_index(int row, int col, int num_col) {
+  return row * num_col + col;
+}
+
 
 int main(int argc, char* argv[]) {
   int size = 480;
@@ -29,10 +49,77 @@ int main(int argc, char* argv[]) {
   printf("  X range = [%.4f,%.4f]\n", xmin, xmax);
   printf("  Y range = [%.4f,%.4f]\n", ymin, ymax);
 
-  // todo: your work here
   // generate pallet
   srand(time(0));
-
-  // compute image
-
+  // allocate memory for the pixels of the image
+  struct ppm_pixel* image = malloc(sizeof(struct ppm_pixel) * size * size);
+  // generate color pallette p
+  struct ppm_pixel* p = malloc(sizeof(struct ppm_pixel) * maxIterations);
+  unsigned char baser = rand() % 255;
+  unsigned char baseg = rand() % 255;
+  unsigned char baseb = rand() % 255;
+  for (int i = 0; i < maxIterations; i++) {
+    p[i].red = baser + rand() % 100 - 50;
+    p[i].green = baseg + rand() % 100 - 50;
+    p[i].blue = baseb + rand() % 100 - 50;
+  }
+  // start timer
+  double timer;
+  struct timeval tstart, tend;
+  gettimeofday(&tstart, NULL);
+  // start calculating the colors
+  int r = 0;
+  int c = 0;
+  for (r = 0; r < size; r++) {
+    for (c = 0; c < size; c++) {
+      float xfrac = r / (float) size;
+      float yfrac = c / (float) size;
+      float x0 = xmin + xfrac * (xmax - xmin);
+      float y0 = ymin + yfrac * (ymax - ymin);
+      float x = 0.0;
+      float y = 0.0;
+      int iter = 0;
+      while (iter < maxIterations && x * x + y * y < 2 * 2) {
+        float xtmp = x * x - y * y + x0;
+        y = 2 * x * y + y0;
+        x = xtmp;
+        iter++;
+      }
+      if (iter < maxIterations) { // diverge, paint color
+        image[find_index(r, c ,size)].red = p[iter].red;
+        image[find_index(r, c ,size)].green = p[iter].green;
+        image[find_index(r, c ,size)].blue = p[iter].blue;
+      } else { // converge, paint black
+        image[find_index(r, c ,size)].red = 0;
+        image[find_index(r, c ,size)].green = 0;
+        image[find_index(r, c ,size)].blue = 0;
+      }
+    }
+  }
+  // end timer
+  gettimeofday(&tend, NULL);
+  timer = tend.tv_sec - tstart.tv_sec + (tend.tv_usec - tstart.tv_usec)
+    / 1.e6;
+  // display time used
+  printf("Computed mandelbrot set (%ix%i) in %f seconds\n", 
+      size, size, timer);
+  // generate file name
+  char filename[128] = "mandelbrot-";
+  char size_str[16] = "";
+  sprintf(size_str, "%d", size);
+  strcat(filename, size_str);
+  strcat(filename, "-");
+  // generating time stamp
+  int stamp = time(0);
+  char time_stamp[16] = "";
+  sprintf(time_stamp, "%d", stamp);
+  strcat(filename, time_stamp);
+  strcat(filename, ".ppm");
+  // write file
+  printf("Writing file: %s\n", filename);
+  write_ppm(filename, image, size, size);
+  // freeing variables
+  free(image);
+  free(p);
+  return 0;
 }
--- A08/warmup.c
+++ A08/warmup.c
@@ -1,5 +1,36 @@
+/**
+ * Author: Louis Jiang
+ * Date: Mar 21, 2023
+ * Description: A program that generates 3 processes, each says Bye when the
+ * process exits.
+ */
+#include <stdlib.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 int main() {
+  // A0
+  int current_pid = getpid();
+  printf("%i  A0\n", current_pid);
+  // B0 & B1
+  pid_t processB = fork();
+  if (processB) {
+    current_pid = getpid();
+    printf("%i  B0\n", current_pid);
+    pid_t processC = fork();
+    if (processC == 0) {
+      current_pid = getpid();
+      printf("%i  C1\n", current_pid);
+    } else {
+      current_pid = getpid();
+      printf("%i  C0\n", current_pid);
+    }
+  } else {
+    current_pid = getpid();
+    printf("%i  B1\n", current_pid);
+  }
+  current_pid = getpid();
+  printf("%i  Bye\n", current_pid);
   return 0;
 }
