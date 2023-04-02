71e94388843a3e3b2c54e5874ef3b3d17b96d73d
Ubuntu <ljiang1@haverford.edu>
2023-04-02 13:44:14 -0400

    DONE

--- A09/search.c
+++ A09/search.c
@@ -1,11 +1,85 @@
+/**
+ * Author: Louis Jiang
+ * Date: Apr 1, 2023
+ * Description:
+ * A program that uses N threads to search for a value in a list.
+ */
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+
+struct target_range {
+  int st;
+  int ed;
+  int target;
+  int* arr;
+  int t_num;
+  int* found;
+};
+
+void* thread_search(void* args) {
+  struct target_range* tr = (struct target_range*) args;
+  for (int i = tr->st; i < tr->ed; i++) {
+    if (tr->target == tr->arr[i]) {
+      printf("Thread %i found %i at index %i\n", tr->t_num, tr->target,i);
+      *tr->found = 1;
+      break;
+    }
+  }
+  pthread_exit(NULL);
+  return NULL;
+}
 
 int main(int argc, char** argv) {
   if (argc < 2) {
     printf("usage: %s <NumThreads>\n", argv[0]);
     exit(0);
   }
-
+  int num_threads = atoi(argv[1]);
+  // read in file
+  FILE* fp = fopen("data.bin", "rb");
+  int length; // the total number of integers in the data list
+  fread(&length, sizeof(int), 1, fp);
+  int* arr = malloc(sizeof(int) * length);
+  fread(arr, sizeof(int), length, fp);
+  fclose(fp);
+  // user interface
+  printf("Enter a value to search: ");
+  int target;
+  scanf(" %i", &target);
+  // start searching
+  int found = 0; // 0 for not found, 1 for found
+  // generating threads
+  pthread_t* threads = malloc(sizeof(pthread_t) * num_threads);
+  // creating info structs
+  struct target_range* tr = malloc(sizeof(struct target_range) * num_threads);
+  int st = 0;
+  int ed = length / num_threads;
+  for (int i = 0; i < num_threads; i++) {
+    if (i == num_threads - 1) {
+      ed = length;
+    }
+    // assigning values into the info struct
+    tr[i].st = st;
+    tr[i].ed = ed;
+    tr[i].target = target;
+    tr[i].arr = arr;
+    tr[i].t_num = i + 1;
+    tr[i].found = &found;
+    // spliting threads
+    pthread_create(&threads[i], NULL, thread_search, &tr[i]);
+    st += length / num_threads;
+    ed += length / num_threads;
+  }
+  // joining the threads
+  for (int i = 0; i < num_threads; i++) {
+    pthread_join(threads[i], NULL);
+  }
+  if (found == 0) {
+    printf("Not found!\n");
+  }
+  // freeing vars
+  free(arr);
+  free(threads);
   return 0;
 }
--- A09/thread_mandelbrot.c
+++ A09/thread_mandelbrot.c
@@ -1,3 +1,12 @@
+/**
+ * Author: Louis Jiang
+ * Date: Apr 1, 2023
+ * Description:
+ * A program that outputs a PPM image of the mandelbrot set using 
+ * multi-threading
+ */
+
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -6,6 +15,69 @@
 #include <pthread.h>
 #include "read_ppm.h"
 #include "write_ppm.h"
+#include <sys/time.h>
+
+/**
+ * Find the index of the pixel in the array of pixels with its row number
+ * and column number, given the number of columns of the array.
+ * @param row the row number
+ * @param col the column number
+ * @param num_col the number of columns in the array
+ * @return the index of the pixel
+ */
+int find_index(int row, int col, int num_col) {
+  return row * num_col + col;
+}
+
+struct mand_data {
+  struct ppm_pixel* image;
+  int x_st;
+  int x_ed;
+  int y_st;
+  int y_ed;
+  int size;
+  float xmin;
+  float xmax;
+  float ymin;
+  float ymax;
+  int maxIterations;
+  struct ppm_pixel* p;
+};
+
+/**
+ * thread function
+ */
+void* thread_func(void* id) {
+  struct mand_data* md = (struct mand_data*) id;
+  // start calculating the colors
+  for (int r = md->y_st; r < md->y_ed; r++) {
+    for (int c = md->x_st; c < md->x_ed; c++) {
+      float xfrac = c / (float) md->size;
+      float yfrac = r / (float) md->size;
+      float x0 = md->xmin + xfrac * (md->xmax - md->xmin);
+      float y0 = md->ymin + yfrac * (md->ymax - md->ymin);
+      float x = 0.0;
+      float y = 0.0;
+      int iter = 0;
+      while (iter < md->maxIterations && x * x + y * y < 2 * 2) {
+        float xtmp = x * x - y * y + x0;
+        y = 2 * x * y + y0;
+        x = xtmp;
+        iter++;
+      }
+      if (iter < md->maxIterations) { // diverge, paint color
+        md->image[find_index(r, c, md->size)].red = md->p[iter].red;
+        md->image[find_index(r, c ,md->size)].green = md->p[iter].green;
+        md->image[find_index(r, c ,md->size)].blue = md->p[iter].blue;
+      } else { // converge, paint black
+        md->image[find_index(r, c, md->size)].red = 0;
+        md->image[find_index(r, c, md->size)].green = 0;
+        md->image[find_index(r, c, md->size)].blue = 0;
+      }
+    }
+  }
+  return NULL;
+}
 
 int main(int argc, char* argv[]) {
   int size = 480;
@@ -33,7 +105,82 @@ int main(int argc, char* argv[]) {
   printf("  X range = [%.4f,%.4f]\n", xmin, xmax);
   printf("  Y range = [%.4f,%.4f]\n", ymin, ymax);
 
-  // todo: your code here
+  // set seed for the random number
+  srand(time(0));
+  // create image
+  struct ppm_pixel* image = malloc(sizeof(struct ppm_pixel) * size * size);
   // generate pallet
+  struct ppm_pixel* p = malloc(sizeof(struct ppm_pixel) * maxIterations);
+  unsigned char baser = rand() % 255;
+  unsigned char baseg = rand() % 255;
+  unsigned char baseb = rand() % 255;
+  for (int i = 0; i < maxIterations; i++) {
+    p[i].red = baser + rand() % 100 - 50;
+    p[i].green = baseg + rand() % 100 - 50;
+    p[i].blue = baseb + rand() % 100 - 50;
+  }
+  // start timer
+  double timer;
+  struct timeval tstart, tend;
+  gettimeofday(&tstart, NULL);
   // compute image
+  // spliting threads
+  struct mand_data* md = malloc(sizeof(struct mand_data) * numProcesses);
+  pthread_t* threads = malloc(sizeof(pthread_t) * numProcesses);
+  for (int i = 0; i < numProcesses; i++) {
+    md[i].image = image;
+    md[i].size = size;
+    md[i].xmin = xmin;
+    md[i].xmax = xmax;
+    md[i].ymin = ymin;
+    md[i].ymax = ymax;
+    md[i].maxIterations = maxIterations;
+    md[i].p = p;
+    if (i % 2 == 0) {
+      md[i].x_st = 0;
+      md[i].x_ed = size / 2;
+    } else {
+      md[i].x_st = size / 2;
+      md[i].x_ed = size;
+    }
+    if (i - 2 < 0) {
+      md[i].y_st = 0;
+      md[i].y_ed = size / 2;
+    } else {
+      md[i].y_st = size / 2;
+      md[i].y_ed = size;
+    }
+    pthread_create(&threads[i], NULL, thread_func, &md[i]);
+  }
+  // merging threads
+  for (int i = 0; i < numProcesses; i++) {
+    pthread_join(threads[i], NULL);
+  }
+  // end timer
+  gettimeofday(&tend, NULL);
+  timer = tend.tv_sec - tstart.tv_sec + (tend.tv_usec - tstart.tv_usec)
+    / 1.e6;
+  // display time used
+  printf("Computed mandelbrot set (%ix%i) in %f seconds\n",
+      size, size, timer);
+  // generate file name
+  char filename[128] = "mandelbrot-";
+  char size_str[16] = "";
+  sprintf(size_str, "%d", size);
+  strcat(filename, size_str);
+  strcat(filename, "-");
+  // generating time stamp
+  int stamp = time(0);
+  char time_stamp[16] = "";
+  sprintf(time_stamp, "%d", stamp);
+  strcat(filename, time_stamp);
+  strcat(filename, ".ppm");
+  // write file
+  printf("Writing file: %s\n", filename);
+  write_ppm(filename, image, size, size);
+  // freeing variables
+  free(p);
+  free(image);
+  free(threads);
+  return 0;
 }
