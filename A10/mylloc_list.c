58f25cd5a2d30d45f83c013b96ed9f9760a40242
Louis Jiang <louisjiang@Mono.local>
2023-04-23 22:23:57 -0400

    working on it

--- A10/mylloc_list.c
+++ A10/mylloc_list.c
@@ -2,19 +2,60 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+
 struct chunk {
   int size;
   int used;
   struct chunk *next;
 };
+
+
 struct chunk *flist = NULL;
 
-void *malloc (size_t size) {
-  // TODO: Implement malloc with a free list (See reading for details)
-  return NULL;
+
+void* malloc (size_t size) {
+  if (size == 0) {
+    return NULL;
+  }
+  struct chunk* next = flist;
+  struct chunk* prev = NULL;
+  while (next != NULL) {
+    if (next->size >= size) { // this chunk has greater/equal size
+      if (prev != NULL) { // not the first chunk in the list
+        // remove the chunk from the list
+        prev->next = next->next;
+      } else { // the first chunk in the list
+        // remove the first chunk
+        flist = next->next;
+      }
+      next->used = size;
+      return (void*) next + 1; // return the chunk
+    } else { // this chunk is smaller than size
+      // go to next chunk
+      prev = next;
+      next = next->next;
+    }
+  }
+  // if the program runs through here, no chunk has suitable size
+  // we allocate new memory using sbrk()
+  void* memory = sbrk(size + sizeof(struct chunk));
+  if (memory == (void*) -1) {
+    return NULL; // falied to sbrk
+  } else {
+    struct chunk* cnk = (struct chunk*) memory;
+    cnk->size = size;
+    cnk->used = size;
+    return (void*) (cnk + 1);
+  }
 }
 
-void free(void *memory) {
-  // TODO: Implement malloc with a free list (See reading for details)
+
+void free(void* memory) {
+  if (memory != NULL) {
+    struct chunk* cnk = (struct chunk*) ((struct chunk*) memory - 1);
+    cnk->next = flist;
+    flist = cnk;
+    cnk->used = 0;
+  }
   return;
 }
